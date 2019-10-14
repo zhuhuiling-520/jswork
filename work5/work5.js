97c84b9d7d19ae053591908600481e5142c79cd7
zhuhuiling-520 <3193974492@qq.com>
2019-10-14 20:10:37 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,36 +1,32 @@
-str = '<table border="1">';
-for (let i = 1;i < 10; ++i) {  
-    str += '<tr>';
-    for (var  j = 1;j < 10; ++j){
-        str += '<td>0</td>';
-    }
-    str +='</tr>';
+let str = '<table border="1">'
+for(let i = 1;i < 10; ++i) {
+    str += '<tr>'
+    for (var j = 1;j<10; ++j){
+        str +='<td>&nbsp;</td>'
+    }
+    str +='</tr>'
 }
-str +='</table>';
-document.getElementById('table1').innerHTML = str
+str +='</table>'
+document.getElementById('table1').innerHTML=str
 
-str = '<table border="1">';
-for(let i=9;i>0;--i){
-    str += '<tr>'
-    for(var j=i;j>0;--j){
-        str += '<td>&nbsp;</td>'
-    }
-    str +='</tr>'
+str = '<table border="1">'
+for(let i = 9;i>0; --i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>&nbsp;</td>'  
+    }
+    str +='</tr>'
 }
-str +='</table>'
-document.getElementById('table2').innerHTML = str
+str +='</table>'
+document.getElementById('table2').innerHTML=str
 
-str = '<table border="1">';
-for(let i=10;i>10;--i){
-    str += '<tr>'
-    for(var j=i;j>0;--j){
-        str += '<td>&nbsp;</td>'
-    }
-    str +='</tr>'
+str='<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>' + i + '*' + j + '='+ ( i * j ) +'</td>'
+    }
+    str +='</tr>'
 }
-str +='</table>'
-document.getElementById('table2').innerHTML = str
-
-
-
-
+str += '</table>'
+document.getElementById('table3').innerHTML=str
